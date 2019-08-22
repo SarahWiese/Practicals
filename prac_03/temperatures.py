3935d607174e3f58a02996c3ffc10ed2ae827d12
Sarah <sarah.wiese@my.jcu.edu.au>
2019-08-23 05:43:36 +1000

    "Refactor temperature program to use functions"

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -3,25 +3,38 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
+
+def main():
+    """Program to convert Celsius to Fahrenheit and Fahrenheit to Celsius"""
+    menu = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        fahrenheit = float(input("Fahrenheit:"))
-        celsius = 5 / 9.0 * (fahrenheit - 32)
-        print("Result: {:.2f} C" .format(celsius))
-    else:
-        print("Invalid option")
-    print(MENU)
+    print(menu)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            while not convert_celsius_to_fahrenheit():
+                print("Result: {:.2f} F".format(convert_celsius_to_fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit:"))
+            while not convert_fahrenheit_to_celsius():
+                print("Result: {:.2f} C" .format(convert_fahrenheit_to_celsius))
+        else:
+            print("Invalid option")
+        print(menu)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_celsius_to_fahrenheit():
+    """Function to convert Celcius to Fahrenheit."""
+    fahrenheit = celsius * 9.0 / 5 + 32
+
+
+def convert_fahrenheit_to_celsius():
+    """Function to convert Fahrenheit to Celsius"""
+    celsius = 5 / 9.0 * (fahrenheit - 32)
+
+
+main()
