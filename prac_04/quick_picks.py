cd3456605ebee920a9cf8e6a2eabcff3bff9306b
Sarah <sarah.wiese@my.jcu.edu.au>
2019-08-24 03:12:36 +1000

    "Correct range for random number generation for quick_pick program"

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,7 +4,7 @@ quick_picks = []
 number_of_quick_picks = int(input("How many quick picks?"))
 
 for number_of_quick_pick in range(1, number_of_quick_picks + 1):
-    quick_pick = [random.randint(1, 46) for i in range(6)]
+    quick_pick = [random.randint(1, 45) for i in range(6)]
     quick_pick.sort()
     quick_picks.append(quick_pick)
 for ticket in quick_picks:
