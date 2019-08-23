998da80d10ce82a78fe8cdbe0766a8e407451cc0
Sarah <sarah.wiese@my.jcu.edu.au>
2019-08-24 02:25:20 +1000

    "Update quick_pick lottery ticket generator program so output on separate lines and in ascending order"

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,6 +4,8 @@ quick_picks = []
 number_of_quick_picks = int(input("How many quick picks?"))
 
 for number_of_quick_pick in range(1, number_of_quick_picks + 1):
-    quick_pick = [random.randrange(1, 46, 1) for i in range(6)]
+    quick_pick = [random.randint(1, 46) for i in range(6)]
+    quick_pick.sort()
     quick_picks.append(quick_pick)
-print("{}".format(quick_picks))
+for ticket in quick_picks:
+    print(*ticket)
