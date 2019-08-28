f5aa96fd329e3d38dc5c691ff53b8d314d3bce83
Sarah <sarah.wiese@my.jcu.edu.au>
2019-08-29 09:53:48 +1000

    "Add function to find longest word length and use for width variable."

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -12,7 +12,17 @@ def main():
         else:
             sentence_dict[word] = 1
     for word, count in sentence_dict.items():
-        print("{:5} : {}".format(word, count))
+        print("{:{}} : {}".format(word, max_length_word(sentence), count))
+
+
+def max_length_word(sentence):
+    """Find longest word in sentence."""
+    longest_word = 0
+    for words in sentence:
+        if len(words) > longest_word:
+            longest_word = len(words)
+            print(longest_word)
+        return longest_word
 
 
 main()
